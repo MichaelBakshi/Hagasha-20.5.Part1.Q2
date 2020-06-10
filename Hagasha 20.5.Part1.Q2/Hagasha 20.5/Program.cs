414165d92acd1a338bb325b7e026cf2bd24bb719
MichaelBakshi <62958898+MichaelBakshi@users.noreply.github.com>
2020-06-10 18:08:53 +0300

    Add files via upload

---
+++ Hagasha 20.5.Part1.Q2/Hagasha 20.5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hagasha_20._5
+{
+    class Program
+    {
+        // The function Main won't work since correct expression for an array is "int [] a"
+        // and not "Int [] a"
+        // In case we fix "Int" to "int" in Main function, the first number in array a will
+        // be updated. Because Update function comes after initial definition of the array.
+
+        static void Update(int[] a)
+        {
+            a[0] = 1;
+        }
+        static void Main(string[] args)
+        {
+            Int[] a = { 5, 4, -5 };
+            Update(a);
+
+        }
+       
+    }
+}
